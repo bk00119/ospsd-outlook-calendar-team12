c550a08da96811a52cc52299ab53dd49473ba691
IanC13 <ian.yl.cheng@gmail.com>
2026-03-24 21:36:44 -0400

    Add service client adapter with delete_event implementation

---
+++ src/outlook_service_client_adapter/src/outlook_service_client_adapter/__init__.py
@@ -0,0 +1,5 @@
+"""Adapter that wraps the auto-generated service client with the Client ABC."""
+
+from outlook_service_client_adapter.adapter import ServiceClientAdapter
+
+__all__ = ["ServiceClientAdapter"]
