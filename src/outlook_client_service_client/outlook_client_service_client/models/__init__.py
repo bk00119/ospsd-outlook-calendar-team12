00f6aef5a8a44a8183074ad1131a36b2a4b02eed
Anastasia Strulistova <strulist@Anastasias-MacBook-Pro.local>
2026-03-24 20:05:44 -0400

    Add generated service client and configure lint/type excludes for generated code

---
+++ src/outlook_client_service_client/outlook_client_service_client/models/__init__.py
@@ -0,0 +1,25 @@
+"""Contains all the data models used in inputs/outputs"""
+
+from .callback_auth_callback_get_response_callback_auth_callback_get import (
+    CallbackAuthCallbackGetResponseCallbackAuthCallbackGet,
+)
+from .event_create_request import EventCreateRequest
+from .event_response import EventResponse
+from .event_update_request import EventUpdateRequest
+from .health_check_health_get_response_health_check_health_get import HealthCheckHealthGetResponseHealthCheckHealthGet
+from .http_validation_error import HTTPValidationError
+from .logout_auth_logout_post_response_logout_auth_logout_post import LogoutAuthLogoutPostResponseLogoutAuthLogoutPost
+from .validation_error import ValidationError
+from .validation_error_context import ValidationErrorContext
+
+__all__ = (
+    "CallbackAuthCallbackGetResponseCallbackAuthCallbackGet",
+    "EventCreateRequest",
+    "EventResponse",
+    "EventUpdateRequest",
+    "HTTPValidationError",
+    "HealthCheckHealthGetResponseHealthCheckHealthGet",
+    "LogoutAuthLogoutPostResponseLogoutAuthLogoutPost",
+    "ValidationError",
+    "ValidationErrorContext",
+)
---
+++ src/outlook_client_service_client/outlook_client_service_client/models/event_create_request.py
@@ -0,0 +1,123 @@
+from __future__ import annotations
+
+import datetime
+from collections.abc import Mapping
+from typing import Any, Self, TypeVar, cast
+
+from attrs import define as _attrs_define
+from attrs import field as _attrs_field
+from dateutil.parser import isoparse
+
+from ..types import UNSET, Unset
+
+T = TypeVar("T", bound="EventCreateRequest")
+
+
+@_attrs_define
+class EventCreateRequest:
+    """Request model for creating an event.
+
+    Attributes:
+        title (str):
+        starts_at (datetime.datetime):
+        ends_at (datetime.datetime):
+        location (None | str | Unset):
+        description (None | str | Unset):
+
+    """
+
+    title: str
+    starts_at: datetime.datetime
+    ends_at: datetime.datetime
+    location: None | str | Unset = UNSET
+    description: None | str | Unset = UNSET
+    additional_properties: dict[str, Any] = _attrs_field(init=False, factory=dict)
+
+    def to_dict(self) -> dict[str, Any]:
+        title = self.title
+
+        starts_at = self.starts_at.isoformat()
+
+        ends_at = self.ends_at.isoformat()
+
+        location: None | str | Unset
+        if isinstance(self.location, Unset):
+            location = UNSET
+        else:
+            location = self.location
+
+        description: None | str | Unset
+        if isinstance(self.description, Unset):
+            description = UNSET
+        else:
+            description = self.description
+
+        field_dict: dict[str, Any] = {}
+        field_dict.update(self.additional_properties)
+        field_dict.update(
+            {
+                "title": title,
+                "starts_at": starts_at,
+                "ends_at": ends_at,
+            },
+        )
+        if location is not UNSET:
+            field_dict["location"] = location
+        if description is not UNSET:
+            field_dict["description"] = description
+
+        return field_dict
+
+    @classmethod
+    def from_dict(cls, src_dict: Mapping[str, Any]) -> Self:
+        d = dict(src_dict)
+        title = d.pop("title")
+
+        starts_at = isoparse(d.pop("starts_at"))
+
+        ends_at = isoparse(d.pop("ends_at"))
+
+        def _parse_location(data: object) -> None | str | Unset:
+            if data is None:
+                return data
+            if isinstance(data, Unset):
+                return data
+            return cast("None | str | Unset", data)
+
+        location = _parse_location(d.pop("location", UNSET))
+
+        def _parse_description(data: object) -> None | str | Unset:
+            if data is None:
+                return data
+            if isinstance(data, Unset):
+                return data
+            return cast("None | str | Unset", data)
+
+        description = _parse_description(d.pop("description", UNSET))
+
+        event_create_request = cls(
+            title=title,
+            starts_at=starts_at,
+            ends_at=ends_at,
+            location=location,
+            description=description,
+        )
+
+        event_create_request.additional_properties = d
+        return event_create_request
+
+    @property
+    def additional_keys(self) -> list[str]:
+        return list(self.additional_properties.keys())
+
+    def __getitem__(self, key: str) -> Any:
+        return self.additional_properties[key]
+
+    def __setitem__(self, key: str, value: Any) -> None:
+        self.additional_properties[key] = value
+
+    def __delitem__(self, key: str) -> None:
+        del self.additional_properties[key]
+
+    def __contains__(self, key: str) -> bool:
+        return key in self.additional_properties
